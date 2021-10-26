65d3b863c97ca643325428789043cb73a01fd19e
Elenmanuel1441 <elenmanuel1441@gmail.com>
2021-10-26 14:48:38 -0400

    Cambios menores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { NavController} from  '@ionic/angular';
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import {Storage} from '@ionic/storage';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
