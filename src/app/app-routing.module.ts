c45431791b5cfaa009578de01ba4ad4d70fa08a5
Elenmanuel1441 <elenmanuel1441@gmail.com>
2021-10-26 10:10:14 -0400

    Nuevas paginas
    Se crearon nuevas paginas el login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,6 +43,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'setting',
+    loadChildren: () => import('./setting/setting.module').then( m => m.SettingPageModule)
+  },
 ];
 
 @NgModule({
