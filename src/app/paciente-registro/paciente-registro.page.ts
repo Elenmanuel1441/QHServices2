c5278b6afb2ce6e1263c010e75faeca6d604c15e
Elenmanuel1441 <elenmanuel1441@gmail.com>
2021-11-07 13:25:13 -0400

    Update paciente-registro.page.ts

--- src/app/paciente-registro/paciente-registro.page.ts
+++ src/app/paciente-registro/paciente-registro.page.ts
@@ -28,12 +28,7 @@ export class PacienteRegistroPage implements OnInit {
     
   ) {
     this.getPacientes();
-<<<<<<< Updated upstream
-    setInterval(() => this.getPacientes(), 10000);
-    this.limpiarCampos();
-=======
   //  this.limpiarCampos();
->>>>>>> Stashed changes
    }
 
   addPaciente()
