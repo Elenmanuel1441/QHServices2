290c3cda0b6f618b142f8275fc55c0c910412842
Elenmanuel1441 <elenmanuel1441@gmail.com>
2021-11-26 01:46:25 -0400

    Calculo de formulario reportes

--- src/app/reportes/reportes.page.ts
+++ src/app/reportes/reportes.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-
+import { ApiService } from '../api.service';
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.page.html',
@@ -11,9 +11,26 @@ export class ReportesPage implements OnInit {
 
   condiccion: number = 0;
 
-  constructor(private router: Router, private afAuth: AuthService) { }
+  constructor(private router: Router,
+    private afAuth: AuthService,
+    private _apiservice: ApiService,)
+     {
+      this. getCountSonoAbdoNoAseg();
+      setInterval(() => this.getCountSonoAbdoNoAseg(), 10000);
+
+      this.getCountSonoAbdoSeg();
+      setInterval(() => this.getCountSonoAbdoSeg(), 10000);
+    }
+
+  ReportSonoAbdoNoSeg: any = [];
+  SonoAbdoNoSeg: any;
+
+  ReportSonoAbdoSeg: any = [];
+  SonoAbdoSeg: any;
+
 
   ngOnInit() {
+    this. getCountSonoAbdoNoAseg();
   }
 
 
@@ -32,5 +49,25 @@ export class ReportesPage implements OnInit {
       this.condiccion = 0;
     }
   }
-  
+
+  getCountSonoAbdoNoAseg()
+  {
+      this._apiservice.getCountSonoAbdoNoAseg().subscribe((res:any) => {
+        console.log("SUCCESS ===",res);
+        let ReportSonoAbdoNoSeg = res[0];
+        this.SonoAbdoNoSeg = ReportSonoAbdoNoSeg.resultado;
+        },(error: any) => {
+          console.log("ERROR ===",error);
+        });
+  }
+  getCountSonoAbdoSeg()
+  {
+    this._apiservice.getCountSonoAbdoSeg().subscribe((res:any) => {
+      console.log("SUCCESS ===",res);
+      let ReportSonoAbdoSeg = res[0];
+      this.SonoAbdoSeg = ReportSonoAbdoSeg.resultado;
+      },(error: any) => {
+        console.log("ERROR ===",error);
+      });
+  }
 }
