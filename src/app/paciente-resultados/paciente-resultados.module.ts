d45da1b902744c3f3f8c000923c39d58803d59b3
Elenmanuel1441 <elenmanuel1441@gmail.com>
2021-11-27 23:59:13 -0400

    Se agrego searchbar en entrega de resultados de pacientes y laboratorio

--- src/app/paciente-resultados/paciente-resultados.module.ts
+++ src/app/paciente-resultados/paciente-resultados.module.ts
@@ -8,12 +8,17 @@ import { PacienteResultadosPageRoutingModule } from './paciente-resultados-routi
 
 import { PacienteResultadosPage } from './paciente-resultados.page';
 
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgxPaginationModule} from 'ngx-pagination';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    PacienteResultadosPageRoutingModule
+    PacienteResultadosPageRoutingModule,
+    Ng2SearchPipeModule,
+    NgxPaginationModule
   ],
   declarations: [PacienteResultadosPage]
 })
