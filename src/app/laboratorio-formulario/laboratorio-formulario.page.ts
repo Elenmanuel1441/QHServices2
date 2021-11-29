0937b0b8f479e73c67bd1f4762be5fbb24237398
LuisCastDev <90583642+LuisCastDev@users.noreply.github.com>
2021-11-28 22:56:24 -0400

    trabajando en la impresion de la orina

--- src/app/laboratorio-formulario/laboratorio-formulario.page.ts
+++ src/app/laboratorio-formulario/laboratorio-formulario.page.ts
@@ -30,6 +30,48 @@ export class LaboratorioFormularioPage implements OnInit {
   c19={
     resultado : ''
   }
+  orina={
+    color : '',
+    aspecto : '',
+    olor : '',
+    reaccion : '',
+    densidad : '',
+    proteina : '',
+    glucosa : '',
+    acetona : '',
+    acidoAbsorbico : '',
+    pigsBiliares : '',
+    urobilogeno : '',
+    sangreOculta : '',
+    nitrito : '',
+    piocitos : '',
+    globulosRojos : '',
+    epitelios : '',
+    fibra : '',
+    cilindros : '',
+    cristales : '',
+    Bacteria : '',
+    tVaginales : '',
+    monilias : '',
+    otros : ''    
+  }
+  coprologico={
+    sangreOculta : '',
+    globulosRojos : '',
+    color : '',
+    mucus : '',
+    celulasVeg : '',
+    consistencia : '',
+    huevos : ''
+       
+  }
+  tipificacion={
+    grupo : '',
+    rh : ''
+   
+  }
+  
+
   pdfObj = null;
 
 
@@ -58,6 +100,123 @@ export class LaboratorioFormularioPage implements OnInit {
 
   ngOnInit() {
   }
+  async createOrina(){
+    var orinaDefinition: any = {
+      pageSize: 'LETTER',
+      content:[
+  
+        { text: 'CENTRO  DIAGNOSTICO SAN FERNANDO DE MONTECRISTI \n\n', style: 'header',alignment: 'center' },
+        { text: 'Calle Pedro Pablo Fernández, Esq. Beller, ', style: 'sub-header',alignment: 'center' },
+        { text: 'Montecristi, R.D Tel: 809-579-3314 \n\n ', style: 'sub-header',alignment: 'center' },
+        { text: 'Laboratorio \n\n', style: 'header',alignment: 'center' },
+        {
+          text:  [
+                 {text: new Date().toLocaleDateString(), alignment: 'left'}, '\n\n',
+                 ]
+        },
+  
+        {
+          text:  [
+                 {text: 'PACIENTE:', style: 'subheader'}, this.nombre_paciente +' '+ this.apellido_paciente, '\n\n',
+                ]
+        },
+        {
+          text:  [
+                 {text: 'EDAD:', style: 'subheader'}, this.Edad,'\n\n',
+                ]
+        },
+        // {
+        //   image: await this.getBase64ImageFromURL(
+        //     "https://images.pexels.com/photos/209640/pexels-photo-209640.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300"
+  
+        //   )
+        // } , 
+  
+        { text: 'MEDICO: A QUIEN CORRESPONDA', style: 'subheader'},
+        
+        { text: 'EXAMEN DE ORINA:\n\n', style: 'subheader', alignment: 'center' },
+      	{text: 'but you can provide a custom styler as well', margin: [0, 20, 0, 8]},
+		{
+			style: 'tableExample',
+			table: {
+        widths: [150,100 , 100,100],
+				headerRows: 1,
+				body: [
+					[{text: 'EXAMEN QUIMICO', style: 'tableHeader'}, {text: '', style: 'tableHeader'}, {text: 'EXAMEN MICROSCOPICO', style: 'tableHeader'},{text: '', style: 'tableHeader'}],
+					['Color', this.orina.color, 'Piocitos',this.orina.piocitos],
+         ['Aspecto', this.orina.aspecto, 'Globulos Rojos',this.orina.globulosRojos],
+        [{text:'Examen Quimico',style:'subheader'}, '', 'Fibras',this.orina.fibra],
+          ['Reaccion(PH)', this.orina.reaccion, 'Cilindros',this.orina.cilindros],
+          ['Densidad', this.orina.densidad, 'Cristales',this.orina.cristales],
+          ['Proteinas', this.orina.proteina, 'Bacterias',this.orina.Bacteria],
+          ['Glucosa', this.orina.glucosa, 'T. Vaginales',this.orina.tVaginales],
+          ['Acetona', this.orina.acetona, 'Monilias',this.orina.monilias],
+          ['Acido Ascorbico', this.orina.acidoAbsorbico, '',''],
+          ['Pigs. Biliares', this.orina.pigsBiliares, '',''],
+					['Urobilinogeno', this.orina.urobilogeno, '', ''],
+					['Sangre Oculta', this.orina.sangreOculta, '', ''],
+					['Nitrito', this.orina.nitrito, '', ''],
+					
+				]
+			},
+			layout: {
+				hLineWidth: function (i, node) {
+					return (i === 0 || i === node.table.body.length) ? 2 : 1;
+				},
+				vLineWidth: function (i, node) {
+					return (i === 0 || i === node.table.widths.length) ? 2 : 1;
+				},
+				hLineColor: function (i, node) {
+					return (i === 0 || i === node.table.body.length) ? 'black' : 'gray';
+				},
+				vLineColor: function (i, node) {
+					return (i === 0 || i === node.table.widths.length) ? 'black' : 'gray';
+				},
+			
+			}
+		},
+      ,
+        { text: '________________________', style: 'header',alignment: 'center' },
+        { text: '      BIOANALISTA',alignment: 'center' }
+        
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+        },
+        subheader: {
+          fontSize: 14,
+          bold: true,
+          margin: [0, 15, 0, 0]
+        },
+        story: {
+          italic: true,
+          alignment: 'center',
+          width: '50%',
+        },
+        tableExample: {
+          margin: [0, 5, 0, 15]
+        },
+        tableHeader: {
+          bold: true,
+          fontSize: 13,
+          color: 'black'
+      }
+    }
+  }
+    this.pdfObj = pdfMake.createPdf(orinaDefinition);
+
+
+
+
+
+  }
+
+
+  async createCoprologico(){}
+
+  async createTipificacion(){}
 
 
 
@@ -217,8 +376,8 @@ printPdf() {
   } else {*/
     // On a browser simply use download!
     this.pdfObj.print();
-    this.completeAnalisis(this.id_paciente_analisis);
-    this.router.navigate(['/laboratorio-analisis/',this.id_col_laboratorio])
+   // this.completeAnalisis(this.id_paciente_analisis);
+   // this.router.navigate(['/laboratorio-analisis/',this.id_col_laboratorio])
     this.pdfObj = null;
 }
 
